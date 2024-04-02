e6b2f66f9eda09f9dfd8bb435f96c70164a2ed47
michalshy <91937056+michalshy@users.noreply.github.com>
2024-04-02 17:31:14 +0200

    Initial commit

---
+++ GUI/HelpTexts.hpp
@@ -0,0 +1,11 @@
+//
+// Created by Michin on 02.04.2024.
+//
+#include <string>
+
+#ifndef ASTROKEEPER_HELPTEXTS_HPP
+#define ASTROKEEPER_HELPTEXTS_HPP
+
+const static std::string paramError = "No params needed! Try again";
+
+#endif //ASTROKEEPER_HELPTEXTS_HPP
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "GUI/HelpTexts.hpp"
+
+int main(int argc, char** argv) {
+    if(argc != 1)
+        std::cout<<paramError;
+
+    return 0;
+}
